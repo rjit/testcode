4b5d3710dc242fc83ef472958b9cc0680688b68f
root <root@ip-172-31-40-131.us-west-2.compute.internal>
2018-04-08 04:36:36 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, json, request
+#from flask.ext.mysql import MySQL
+
+app = Flask(__name__)
+ 
+#mysql = MySQL()
+ 
+# MySQL configurations
+#app.config['MYSQL_DATABASE_USER'] = 'root'
+#app.config['MYSQL_DATABASE_PASSWORD'] = 'root123'
+#app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+#mysql.init_app(app)
+
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+@app.route('/showSignUp')
+def showSignUp():
+     return render_template('signup.html')
+
+@app.route('/signUp',methods=['POST'])
+def signUp():
+   # read the posted values from the UI
+   
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+  # validate the received values
+    if _name and _email and _password:
+       return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=5000)
+
